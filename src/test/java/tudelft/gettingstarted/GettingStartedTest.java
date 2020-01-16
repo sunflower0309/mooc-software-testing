d4cd8fb0b3afdc8d97fcbbfe61a2f49599478ed6
sunflower0309 <805786665@qq.com>
2020-01-15 18:43:00 -0800

    2020/1/15

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -13,11 +13,11 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25, result);
+    }
 //
 //    @Test
 //    public void addFiveToMinus20() {
